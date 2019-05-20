6d34ab06e117e55d34daedd0b47c048b1cca6594
Victor Velepucha <vvelepuc@users.noreply.github.com>
2019-05-20 10:58:25 -0500

    Otros cambios

---
+++ InputLoop.cpp
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void Foo() {
+	string prueba;
+	cout << "Hello World!" << endl;
+}
+
+int main(int argc, char** argv) {
+	char input = 0;
+	
+	cout << "Hello! This is a console application." << endl;
+	cout << "Press q to quit, press a to execute foo." << endl;	
+	while(1) {
+		cin >> input;
+		if(input == 'a' || input == 'A' ) {
+			Foo();
+		} else if(input == 'q') {
+			break;
+		} else if(input != '\n') {
+			cout << "Unknown command '" << input << "'! Ignoring...\n";
+		}
+	}
+
+	return 0;
+}
