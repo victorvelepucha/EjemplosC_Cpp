a02210a3fa8cd0c6becaa7dc27e2d78f7bfbbc9e
Victor Velepucha <vvelepuc@users.noreply.github.com>
2019-06-07 13:08:44 -0500

    Actualizacion de ejemplo

--- EjemploArregloLlamadaFuncion.cpp
+++ EjemploArregloLlamadaFuncion.cpp
@@ -1,30 +1,20 @@
 //Ejemplo paso de parametros por referencia en arreglos
 #include <iostream>
-#define TAM_MAX 4
 using namespace std;
 void ImprimirValores(int [],int );
 int main()
 {
-	int valores[TAM_MAX] = {1,2,3,4};
+	int valores[] = {1,2,3,4};
 	int tam = sizeof(valores)/sizeof(valores[0]);
-	cout << tam << endl;
-/*
-	int valores[] = {1,2,3,4,5};
-	
-	int valores[4]; 
-	valores[0] = 1;
-	valores[1] = 2;
-	valores[2] = 3;
-	valores[3] = 4;	
-*/	
-	ImprimirValores(valores,TAM_MAX);
+	cout << sizeof(int) << " " << sizeof(valores) << " " <<  sizeof(valores[0])<< " " << tam << endl;
+	ImprimirValores(valores,tam);
 	return 0;
 }
 
 void ImprimirValores(int a[], int tamanio)
 {
 	int tam = sizeof(a)/sizeof(a[0]);
-	cout << tam << endl;
+	cout << sizeof(int) << " " << sizeof(a) << " " <<  sizeof(a[0]) << " " << tam << endl;
 	for(int i=0; i< tamanio ;i++)
 	{
 		cout << a[i] <<" ";
